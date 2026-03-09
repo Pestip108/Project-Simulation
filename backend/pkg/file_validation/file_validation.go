3ecf5e7022a246040e4c9e660b03244c9fb22b0c
Peter Abboud <abboudp2@gmail.com>
2026-03-09 10:40:33 +0200

    Added zip bomb protection

--- backend/pkg/file_validation/file_validation.go
+++ backend/pkg/file_validation/file_validation.go
@@ -1,6 +1,8 @@
 package filevalidation
 
 import (
+	"archive/zip"
+	"bytes"
 	"errors"
 	"net/http"
 )
@@ -14,11 +16,40 @@ var AllowedTypes = map[string]bool{
 	"application/zip": true,
 }
 
+const MaxUncompressedSize = 500 * 1024 * 1024 // 500MB
+
 func ValidateFileType(fileBytes []byte) error {
 	contentType := http.DetectContentType(fileBytes)
 
 	if !AllowedTypes[contentType] {
 		return errors.New("file type not allowed: " + contentType)
+	}
+
+	// If the file is a ZIP, perform additional validation
+	if contentType == "application/zip" {
+		if err := ValidateZipBomb(fileBytes); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func ValidateZipBomb(data []byte) error {
+
+	r, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		return errors.New("invalid zip file")
+	}
+
+	var totalSize uint64
+
+	for _, f := range r.File {
+		totalSize += f.UncompressedSize64
+
+		if totalSize > MaxUncompressedSize {
+			return errors.New("zip expands too large")
+		}
 	}
 
 	return nil
